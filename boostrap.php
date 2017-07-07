4a06b36cf22d9cb4635a64c298893f509ef36fca
Yohan Courard <yohan.courard@gmail.com>
2017-07-07 14:17:49 +0200

    reset

--- boostrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-function __autoload($class_name) {
-    $aObject = explode("_",$class_name);
-
-    $file = array_pop($aObject);
-
-    require_once 'app/'.implode('/',$aObject).'/'.$file . '.php';
-}
